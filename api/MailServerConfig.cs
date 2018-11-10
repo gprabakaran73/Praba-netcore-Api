50b29f674f9da41c44fa8b9d66c5d7b225f4b6ea
gprabakaran.cgu <gprabakaran.cgu@gmail.com>
2018-11-10 12:07:37 +1100

    checking mail config

---
+++ api/MailServerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api
+{
+    public class MailServerConfig
+    {
+        public MailServerConfig()
+        {
+            Host = "mail";
+            Port = 1025;
+        }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
